89b262121d9bc3ddb24f5b7d9e1462f8deaf2bb5
K.K <zjl.works@gmail.com>
2024-09-25 17:03:17 +0800

    1.change Versions

--- app/tv/build.gradle.kts
+++ app/tv/build.gradle.kts
@@ -21,7 +21,17 @@ android {
         versionCode = Versions.appCode
         versionName = Versions.appName
     }
-
+    applicationVariants.all {
+        val variant = this
+        variant.outputs
+            .map { it as com.android.build.gradle.internal.api.BaseVariantOutputImpl }
+            .forEach { output ->
+                if (variant.buildType.name == "release") {
+                    val outputFileName = "findroid-tv-v${variant.versionName}-${variant.flavorName}-${output.getFilter("ABI")}.apk"
+                    output.outputFileName = outputFileName
+                }
+            }
+    }
     buildTypes {
         named("debug") {
             applicationIdSuffix = ".debug"
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 
 object Versions {
     const val appCode = 28
-    const val appName = "0.15.2"
+    const val appName = "0.15.3"
 
     const val compileSdk = 35
     const val buildTools = "35.0.0"
